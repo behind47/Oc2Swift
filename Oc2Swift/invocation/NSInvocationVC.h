3ed0c6abb99197f7825e88550ab54b70fc2d12a4
behind47 <smalldragen@outlook.com>
2022-07-12 16:35:43 +0800

    抽出一个public 方法，用来执行target对象的aSelector方法，并传入参数arguments

--- Oc2Swift/invocation/NSInvocationVC.h
+++ Oc2Swift/invocation/NSInvocationVC.h
@@ -13,6 +13,16 @@
 
 @interface NSInvocationVC : UIViewController
 
+/// 执行target对象的aSelector方法，并传入参数arguments
+/// @param aSelector 需要执行的方法
+/// @param target 执行方法的对象
+/// @param arguments 方法执行需要的参数列表
++ (id)performSelector:(SEL)aSelector onTarget:(id)target withArguments:(NSArray<id> *)arguments;
+
+/// 将指定类型的void *数据转化成CF对象
+/// @param voidValue   void*数据
+/// @param cfType  要转换成的cf对象的类型
++ (id)obtainCfValue:(void *)voidValue ofType:(const char*)cfType;
 @end
 
 
--- Oc2Swift/invocation/NSInvocationVC.m
+++ Oc2Swift/invocation/NSInvocationVC.m
@@ -5,6 +5,7 @@
 //  Created by behind47 on 2022/7/11.
 //
 /// NSInvocation可以调用类的方法， 这是一种反射调用
+///  据此可以抽出一个public 方法，用来执行target对象的aSelector方法，并传入参数arguments
 
 #import "NSInvocationVC.h"
 
@@ -15,6 +16,71 @@
 @end
 
 @implementation NSInvocationVC
+
+/// 执行target对象的aSelector方法，并传入参数arguments
+/// @param aSelector 需要执行的方法
+/// @param target 执行方法的对象
+/// @param arguments 方法执行需要的参数列表
++ (id)performSelector:(SEL)aSelector onTarget:(id)target withArguments:(NSArray<id> *)arguments {
+    NSMethodSignature *signature = [target methodSignatureForSelector:aSelector];
+    if (signature == nil) {
+        NSLog(@"signature cannot be nil");
+        return nil;
+    }
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    invocation.selector = aSelector;
+    invocation.target = target;
+    NSInteger len = MIN([arguments count], [signature numberOfArguments]);
+    for (int i = 0; i < len; i++) {
+        const char *cfType = [signature getArgumentTypeAtIndex:i+2];
+        id argument = [arguments objectAtIndex:i];
+        // 返回值是结果类型编码后的字符串，因此需要用类型编码后的字符串与其比较，判断是哪一种类型
+        if (strcmp(cfType, @encode(void)) == 0) {
+            [invocation setArgument:&argument atIndex:i+2];
+        } else if (strcmp(cfType, @encode(id)) == 0) { // id是对象
+            [invocation setArgument:&argument atIndex:i+2];
+        } else { // 数值类型
+            if (strcmp(cfType, @encode(BOOL)) == 0) {
+                bool res = [argument boolValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } else if (strcmp(cfType, @encode(char)) == 0) {
+                char res = [argument charValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } else if (strcmp(cfType, @encode(short)) == 0) {
+                short res = [argument shortValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } else if (strcmp(cfType, @encode(int)) == 0) {
+                int res = [argument intValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } else if (strcmp(cfType, @encode(long)) == 0) {
+                long res = [argument longValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } else if (strcmp(cfType, @encode(long long)) == 0) {
+                long long res = [argument longLongValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } else if (strcmp(cfType, @encode(float)) == 0) {
+                float res = [argument floatValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } else if (strcmp(cfType, @encode(double)) == 0) {
+                double res = [argument doubleValue];
+                [invocation setArgument:&res atIndex:i+2];
+            } // 还有一堆无符号类型
+            else {
+                [invocation setArgument:&argument atIndex:i+2];
+            }
+        }
+    }
+    [invocation invoke];
+    if ([signature methodReturnLength] > 0) {
+        void *returnValue = (void *)malloc([signature methodReturnLength]);
+        [invocation getReturnValue:returnValue];
+        id result = [NSInvocationVC obtainCfValue:returnValue ofType:[signature methodReturnType]];
+        free(returnValue);
+        return result;
+    } else {
+        return nil;
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -52,62 +118,51 @@
 }
 
 - (void)foo3:(UIButton *)sender {
-    NSMethodSignature *signature = [self methodSignatureForSelector:@selector(bar3:andBook:andFood:)];
-    if (signature == nil) {
-        NSLog(@"signature cannot be nil");
-        return;
+    [NSInvocationVC performSelector:@selector(bar3:andBook:andFood:) onTarget:self withArguments:@[@"Alan", @"LonelyThroughHunderedOfYears", @(3)]];
+}
+
+/// 将指定类型的void *数据转化成CF对象
+///  @param voidValue   void*数据
+///  @param cfType  要转换成的cf对象的类型
++ (id)obtainCfValue:(void *)voidValue ofType:(const char*)cfType {
+    id result = nil;
+    // 返回值是结果类型编码后的字符串，因此需要用类型编码后的字符串与其比较，判断是哪一种类型
+    if (strcmp(cfType, @encode(void)) == 0) {
+        result = nil;
+        NSLog(@"invocation result: %@", result);
+    } else if (strcmp(cfType, @encode(id)) == 0) { // id是对象
+        result = (__bridge id)(voidValue);
+        NSLog(@"invocation result: %@", result);
+    } else { // 数值类型
+        if (strcmp(cfType, @encode(BOOL)) == 0) {
+            result = [NSNumber numberWithBool:*((BOOL *) voidValue)];
+            NSLog(@"invocation result: %d", [result boolValue]);
+        } else if (strcmp(cfType, @encode(char)) == 0) {
+            result = [NSNumber numberWithChar:*((char *) voidValue)];
+            NSLog(@"invocation result: %c", [result charValue]);
+        } else if (strcmp(cfType, @encode(short)) == 0) {
+            result = [NSNumber numberWithShort:*((short *) voidValue)];
+            NSLog(@"invocation result: %d", [result shortValue]);
+        } else if (strcmp(cfType, @encode(int)) == 0) {
+            result = [NSNumber numberWithInt:*((int *) voidValue)];
+            NSLog(@"invocation result: %d", [result intValue]);
+        } else if (strcmp(cfType, @encode(long)) == 0) {
+            result = [NSNumber numberWithLong:*((long *) voidValue)];
+            NSLog(@"invocation result: %ld", [result longValue]);
+        } else if (strcmp(cfType, @encode(long long)) == 0) {
+            result = [NSNumber numberWithLongLong:*((long long *) voidValue)];
+            NSLog(@"invocation result: %lld", [result longLongValue]);
+        } else if (strcmp(cfType, @encode(float)) == 0) {
+            result = [NSNumber numberWithFloat:*((float *) voidValue)];
+            NSLog(@"invocation result: %f", [result floatValue]);
+        } else if (strcmp(cfType, @encode(double)) == 0) {
+            result = [NSNumber numberWithDouble:*((double *) voidValue)];
+            NSLog(@"invocation result: %f", [result doubleValue]);
+        } // 还有一堆无符号类型
     }
-    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
-    [invocation setTarget:self];
-    [invocation setSelector:@selector(bar3:andBook:andFood:)];
-    NSString *name = @"Alan";
-    [invocation setArgument:&name atIndex:2]; // argument需要传入地址才能正确取值
-    [invocation setArgument:@"LonelyThroughHunderedOfYears" atIndex:3];
-    int food = 3;
-    [invocation setArgument:&food atIndex:4];
-    [invocation invoke];
-    if (signature.methodReturnLength > 0) {
-        const char *resultType = signature.methodReturnType;
-        // 返回值是结果类型编码后的字符串，因此需要用类型编码后的字符串与其比较，判断是哪一种类型
-        if (strcmp(resultType, @encode(void)) == 0) {
-            NSLog(@"invocation result: nil");
-        } else if (strcmp(resultType, @encode(id)) == 0) { // id是对象
-            id result;
-            [invocation getReturnValue:&result];
-            NSLog(@"invocation result: %@", result);
-        } else { // 数值类型
-            void *returnValue = (void *)malloc(signature.methodReturnLength);
-            [invocation getReturnValue:returnValue];
-            id result;
-            // TODO:怎么把NSLog抽出来，能统一处理所有的结果？或者怎么把众多if else去掉？
-            if (strcmp(resultType, @encode(BOOL)) == 0) {
-                result = [NSNumber numberWithBool:*((BOOL *) returnValue)];
-                NSLog(@"invocation result: %d", [result boolValue]);
-            } else if (strcmp(resultType, @encode(char)) == 0) {
-                result = [NSNumber numberWithChar:*((char *) returnValue)];
-                NSLog(@"invocation result: %c", [result charValue]);
-            } else if (strcmp(resultType, @encode(short)) == 0) {
-                result = [NSNumber numberWithShort:*((short *) returnValue)];
-                NSLog(@"invocation result: %d", [result shortValue]);
-            } else if (strcmp(resultType, @encode(int)) == 0) {
-                result = [NSNumber numberWithInt:*((int *) returnValue)];
-                NSLog(@"invocation result: %d", [result intValue]);
-            } else if (strcmp(resultType, @encode(long)) == 0) {
-                result = [NSNumber numberWithLong:*((long *) returnValue)];
-                NSLog(@"invocation result: %ld", [result longValue]);
-            } else if (strcmp(resultType, @encode(long long)) == 0) {
-                result = [NSNumber numberWithLongLong:*((long long *) returnValue)];
-                NSLog(@"invocation result: %lld", [result longLongValue]);
-            } else if (strcmp(resultType, @encode(float)) == 0) {
-                result = [NSNumber numberWithFloat:*((float *) returnValue)];
-                NSLog(@"invocation result: %f", [result floatValue]);
-            } else if (strcmp(resultType, @encode(double)) == 0) {
-                result = [NSNumber numberWithDouble:*((double *) returnValue)];
-                NSLog(@"invocation result: %f", [result doubleValue]);
-            } // 还有一堆无符号类型
-            free(returnValue);
-        }
-    }
+    // TODO:怎么把NSLog抽出来，能统一处理所有的结果？或者怎么把众多if else去掉？
+    
+    return result;
 }
 
 - (int)bar3:(NSString *)name andBook:(NSString *)book andFood:(int)food {
