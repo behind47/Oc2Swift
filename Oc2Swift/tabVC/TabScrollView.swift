f30542fd3175929344e6e7864badd9bcc1e5feeb
behind47 <smalldragen@outlook.com>
2022-07-20 16:49:46 +0800

    tabVCs拖动回弹和换页

--- Oc2Swift/tabVC/TabScrollView.swift
+++ Oc2Swift/tabVC/TabScrollView.swift
@@ -2,8 +2,9 @@
 //  TabScrollView.swift
 //  Oc2Swift
 //
-//  Created by flannchen on 2022/7/20.
+//  Created by behind47 on 2022/7/20.
 //
+/// tabVCs可以滚动，小于30%回弹，超过30%切换
 
 import Foundation
 
@@ -21,6 +22,7 @@ open class TabScrollView : UIScrollView, UIScrollViewDelegate {
     }
     
     func commonInit() {
+        self.delegate = self
         self.addSubview(contentView)
         contentView.snp.makeConstraints { make in
             make.edges.equalTo(self)
@@ -51,5 +53,34 @@ open class TabScrollView : UIScrollView, UIScrollViewDelegate {
             make.top.height.width.equalTo(self)
             make.right.equalTo(contentView)
         }
+    }
+    
+    // MARK: UIScrollViewDelegate
+
+    /// 流程有两个：
+    /// 1. 拖动停止后，如果有惯性，则异步停止惯性滚动。惯性滚动停止后，设置滚动换页动画
+    /// 2. 如果没有惯性，则直接设置滚动换页动画
+    ///
+    /// TODO：回弹的速度太快了，怎么处理？
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        // tabVCs可以滚动，小于50%左弹，超过50%右弹
+        // 首先取消惯性滚动
+        if (decelerate) {
+            DispatchQueue.main.async {
+                scrollView.setContentOffset(CGPoint(x: round(scrollView.contentOffset.x / self.bounds.width) * self.bounds.width, y: 0), animated: false)
+            }
+        } else {
+            // 以scrollView的宽度为计量单位，将contentOffset四舍五入
+            // 拖动事件发生时，布局已经完成，因此认为self.bounds.width是scrollView的宽度
+            scrollView.setContentOffset(CGPoint(x: round(scrollView.contentOffset.x / self.bounds.width) * self.bounds.width, y: 0),
+                                        animated: true)
+        }
+    }
+    
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        // 以scrollView的宽度为计量单位，将contentOffset四舍五入
+        // 拖动事件发生时，布局已经完成，因此认为self.bounds.width是scrollView的宽度
+        scrollView.setContentOffset(CGPoint(x: round(scrollView.contentOffset.x / self.bounds.width) * self.bounds.width, y: 0),
+                                    animated: true)
     }
 }
