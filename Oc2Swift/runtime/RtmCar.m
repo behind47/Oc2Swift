e8d508ed7e2246e015182a2473c41e260f2fafb0
behind47 <smalldragen@outlook.com>
2022-07-21 15:09:39 +0800

    @property属性声明，声明了内部成员变量及其getter/setter方法， 对于低版本的xcode和重写了getter/setter方法的情况，还需要在implementaion中添加@synthesize将属性声明的方法绑定到成员变量上

--- Oc2Swift/runtime/RtmCar.m
+++ Oc2Swift/runtime/RtmCar.m
@@ -9,4 +9,15 @@
 #import "RtmCar.h"
 @implementation RtmCar
 
+/// 如果getter和setter方法都重写了，就需要用\@synthesize指明属性的内部成员变量，将属性声明的方法绑定到内部成员变量上。
+/// 这里也可以把speed绑定到其他成员变量比如carType上
+@synthesize speed = _speed;
+
+- (NSInteger)speed {
+    return _speed;
+}
+
+- (void)setSpeed:(NSInteger)speed {
+    _speed = speed;
+}
 @end
