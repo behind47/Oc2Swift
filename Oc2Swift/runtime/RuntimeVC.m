9fd529c77c002a9f9e5c9869581ce16eb0096e91
behind47 <smalldragen@outlook.com>
2022-07-25 11:15:55 +0800

    weak引用测试，智能指针每次relase的时候检查是否需要释放对象

--- Oc2Swift/runtime/RuntimeVC.m
+++ Oc2Swift/runtime/RuntimeVC.m
@@ -24,6 +24,25 @@
     NSLog(@"type = %@, speed = %ld", car.carType, car.speed);
     RtmCarColor *color = [car.carColorArr firstObject];
     NSLog(@"color = %@", color);
+    
+    RtmCar __weak *a = [RtmCar new];
+    a.speed = 123;
+    NSLog(@"behind%ld", (long)a.speed); // 打出来是0，因为weak对象定义后就被释放了
+    id strongB = [RtmCar new];
+    RtmCar __weak *b = strongB;
+    b.speed = 123;
+    NSLog(@"behind%ld", (long)b.speed); // 打出来是123，因为在对象上增加了一个strong引用，保持了对象
+    RtmCar __weak *c;
+    {
+        id strongC = [RtmCar new];
+        c = strongB;
+        c.speed = 123;
+    }
+    NSLog(@"behind%ld", (long)c.speed); // 打出来是0，因为strongC超出作用域后自动释放引用，于是weak对象自动释放
+    
+    NSError *error = nil;
+    NSError * __strong *pError = &error;
+    
 }
 
 @end
