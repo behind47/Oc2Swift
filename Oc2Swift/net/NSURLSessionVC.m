8bc14b6e5d0b1f997f47d292025874dc48c4b6ef
behind47 <smalldragen@outlook.com>
2022-08-04 20:31:14 +0800

    downloadTask 断点续传

--- Oc2Swift/net/NSURLSessionVC.m
+++ Oc2Swift/net/NSURLSessionVC.m
@@ -11,15 +11,17 @@
 #import <Foundation/Foundation.h>
 #import "NSURLSessionVC.h"
 
-@interface NSURLSessionVC() <NSURLSessionTaskDelegate, NSURLSessionDataDelegate>
-
+@interface NSURLSessionVC() <NSURLSessionTaskDelegate, NSURLSessionDataDelegate, NSURLSessionDownloadDelegate>
+@property (nonatomic, strong) NSURL *downloadURL;
+@property (nonatomic, strong) NSURLSession *session; // 复用session就是复用TCP连接，这是HTTP2.0新特性
 @end
 
 @implementation NSURLSessionVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self testDownload];
+//    [self testDownload];
+    [self testDotDownloadTask];
 }
 
 - (void)testDownload {
@@ -43,6 +45,48 @@
     [task resume];
 }
 
+/// 断点续传
+- (void)testDotDownloadTask {
+    self.downloadURL = [NSURL URLWithString:@"https://bkimg.cdn.bcebos.com/pic/f9dcd100baa1cd11397333cabf12c8fcc2ce2d16?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U4MA==,g_7,xp_5,yp_5/format,f_auto"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.downloadURL];
+    NSURLSessionDownloadTask *task;
+    NSString *library = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, true).firstObject;
+    NSString *resumePath = [library stringByAppendingPathComponent:[self.downloadURL relativePath]];
+    NSData *resumeData = [[NSData alloc] initWithContentsOfFile:resumePath];
+    if (resumeData) {
+        task = [self.session downloadTaskWithResumeData:resumeData]; // 使用之前存放的resumeData可以进行断点续传
+    }
+    /// completionHandler == nil 时才走delegate的回调方法
+    if (!task) {
+#if 1
+        task = [self.session downloadTaskWithRequest:request];
+#else
+        task = [session downloadTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            // handler的优先级高于delegate的回调方法
+            NSLog(@"%s %s", __FILE__ ,__FUNCTION__);
+        }];
+#endif
+    }
+    [task resume];
+    // 取消下载task时可以将断点下载的信息存到resumeData里，暂存到本地，后面通过这个文件来断点续传
+//    [task cancelByProducingResumeData:^(NSData * _Nullable resumeData) {
+//        [resumeData writeToFile:resumePath atomically:true];
+//    }];
+}
+
+// MARK: getter
+- (NSURLSession *)session {
+    if (!_session) {
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        [config setTimeoutIntervalForRequest:60];
+        _session = [NSURLSession sessionWithConfiguration:config
+                                                              delegate:self
+                                                         delegateQueue:[NSOperationQueue mainQueue]];
+    }
+    return _session;
+}
+
 // MARK: NSURLSessionDataDelegate
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
     NSLog(@"%s %s", __FILE__ ,__FUNCTION__);
@@ -56,6 +100,17 @@
 // MARK: NSURLSessionTaskDelegate
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
     NSLog(@"%s %s", __FILE__ ,__FUNCTION__);
+}
+
+// MARK: NSURLSessionDownloadDelegate
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
+    CGFloat progress = totalBytesWritten / totalBytesExpectedToWrite;
+    NSLog(@"%s %s %f", __FILE__, __FUNCTION__, progress);
+    
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
+    
 }
 
 @end
