35a34b55c510e2ed8ecc89c1ea9a4a8a607a73da
behind47 <smalldragen@outlook.com>
2023-05-11 20:50:39 +0800

    试了下appdelegate的生命周期，只有启动的didFinishLaunchingWithOptions会触发

--- Oc2Swift/AppDelegate.swift
+++ Oc2Swift/AppDelegate.swift
@@ -15,6 +15,7 @@ public class AppDelegate: FlutterAppDelegate {
 
     lazy var flutterEngine = FlutterEngine(name: "behind47 engine")
 
+    /// 程序载入后
     public override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         #if DEBUG
@@ -31,7 +32,68 @@ public class AppDelegate: FlutterAppDelegate {
         #endif
 //        GeneratedPluginRegistrant.register(with: self)
         self.flutterEngine.run()
+        print("程序载入后\(#function)")
         return true
+    }
+    
+    /// app将要进入非活动状态时执行，在此期间，app不接收事件
+    public override func applicationWillResignActive(_ application: UIApplication) {
+        super.applicationWillResignActive(application)
+        print("app将要进入非活动状态时执行，在此期间，app不接收事件\(#function)")
+    }
+    
+    /// app进入活动状态时执行
+    public override func applicationDidBecomeActive(_ application: UIApplication) {
+        super.applicationDidBecomeActive(application)
+        print("app进入活动状态时执行\(#function)")
+    }
+    
+    /// app进入后台
+    public override func applicationDidEnterBackground(_ application: UIApplication) {
+        super.applicationDidEnterBackground(application)
+        print("app进入后台\(#function)")
+    }
+    
+    /// app进入前台
+    public override func applicationWillEnterForeground(_ application: UIApplication) {
+        super.applicationWillEnterForeground(application)
+        print("app进入前台\(#function)")
+    }
+    
+    /// app将要退出时，常用于保存数据和一些退出前的清理工作
+    public override func applicationWillTerminate(_ application: UIApplication) {
+        super.applicationWillTerminate(application)
+        print("app将要退出时，常用于保存数据和一些退出前的清理工作\(#function)")
+    }
+    
+    /// statusBar即将变化时
+    public override func application(_ application: UIApplication, willChangeStatusBarFrame newStatusBarFrame: CGRect) {
+        super.application(application, willChangeStatusBarFrame: newStatusBarFrame)
+        print("statusBar即将变化时\(#function)")
+    }
+ 
+    /// statusBar变化完成后执行
+    public override func application(_ application: UIApplication, didChangeStatusBarFrame oldStatusBarFrame: CGRect) {
+        super.application(application, didChangeStatusBarFrame: oldStatusBarFrame)
+        print("statusBar变化完成后执行\(#function)")
+    }
+    
+    /// statusBar方向将要改变时
+    public override func application(_ application: UIApplication, willChangeStatusBarOrientation newStatusBarOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        super.application(application, willChangeStatusBarOrientation: newStatusBarOrientation, duration: duration)
+        print("statusBar方向将要改变时\(#function)")
+    }
+    
+    /// statusBar方向改变后执行
+    public override func application(_ application: UIApplication, didChangeStatusBarOrientation oldStatusBarOrientation: UIInterfaceOrientation) {
+        super.application(application, didChangeStatusBarOrientation: oldStatusBarOrientation)
+        print("statusBar方向改变后执行\(#function)")
+    }
+    
+    /// 系统时间改变时执行
+    public override func applicationSignificantTimeChange(_ application: UIApplication) {
+        super.applicationSignificantTimeChange(application)
+        print("系统时间改变时执行\(#function)")
     }
 
     // MARK: UISceneSession Lifecycle
